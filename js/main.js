8ce86d5ba2a5f95bbd60694f143b5d544c0bf946
Amaryllis1204 <amaryllis1204@gmail.com>
2021-10-16 13:47:48 +0900

    削除機能を実装

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 document.getElementById('add').addEventListener('click', ()=>{
     let todo = document.getElementById('todo').value;
     let add_todo = document.createElement('li');
+    let remove_button = document.createElement('button');
+    remove_button.classList.add('btn' ,'btn-secondary');
+    remove_button.textContent = '削除';
     add_todo.textContent = todo;
+    add_todo.append(remove_button);
     document.getElementsByClassName('todo_list')[0].appendChild(add_todo);
+
+    remove_button.addEventListener('click', ()=>{
+        removeTasks(remove_button);
+    })
 });
+
+const removeTasks = (remove_button) =>{
+    const chosenTask = remove_button.closest('li');
+    document.getElementsByClassName('todo_list')[0].removeChild(chosenTask);
+}
