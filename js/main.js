840d03b5602259c5ed2456a7f55a188c5397f265
Amaryllis1204 <amaryllis1204@gmail.com>
2021-10-16 14:11:07 +0900

    画面幅の調整

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
+//追加ボタンを押したときの処理
 document.getElementById('add').addEventListener('click', ()=>{
     let todo = document.getElementById('todo').value;
     let add_todo = document.createElement('li');
+    
+    //削除ボタンの付与
     let remove_button = document.createElement('button');
     remove_button.classList.add('btn' ,'btn-secondary');
     remove_button.textContent = '削除';
@@ -8,11 +11,13 @@ document.getElementById('add').addEventListener('click', ()=>{
     add_todo.append(remove_button);
     document.getElementsByClassName('todo_list')[0].appendChild(add_todo);
 
+    //削除ボタンを押した時の処理
     remove_button.addEventListener('click', ()=>{
         removeTasks(remove_button);
     })
 });
 
+//削除処理
 const removeTasks = (remove_button) =>{
     const chosenTask = remove_button.closest('li');
     document.getElementsByClassName('todo_list')[0].removeChild(chosenTask);
